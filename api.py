0dcaec837b626413f0e50ed067dc8f3bf4407dca
Tony Cannistra <tony.cannistra@gmail.com>
2022-12-26 09:49:58 -0500

    add last updated field

--- api.py
+++ api.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 from fastapi import HTTPException
 from pydantic import BaseModel
 from s3records import S3Records
+from datetime import datetime
+from typing import Optional
+from typing import List
+from copy import deepcopy
 
 
 S3_STORE = "s3://avymail/records.txt"
@@ -10,8 +14,15 @@ class Recipient(BaseModel):
     email: str
     center: str
     zone_id: str
+    data_last_updated_time: Optional[datetime]
+
+def remove_timestamps(data: List[Recipient]) -> List[Recipient]:
+    for d in data:
+        d['data_last_updated_time'] = None
+    return data
 
 app = FastAPI()
+
 
 @app.post('/add', status_code=201)
 async def add_recipient(r: Recipient):
@@ -24,11 +35,16 @@ async def add_recipient(r: Recipient):
 async def remove_recipient(r: Recipient):
     db = S3Records(S3_STORE)
     try:
-        idx = db.data.index(r.dict())
+        r.data_last_updated_time = None
+        data_copied = deepcopy(db.data)
+        data_copied = remove_timestamps(data_copied)
+        idx = data_copied.index(r.dict())
         del db.data[idx]
         db.save()
     except ValueError:
         raise HTTPException(status_code=404, detail=f"{r.json()} not found")
+    
+    return {"message": "success"}
     
         
 
