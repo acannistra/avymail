d194c9c430f0ffa07a11c8d1d58f902cde40b287
Tony Cannistra <tony.cannistra@gmail.com>
2022-12-26 13:35:57 -0500

    get javascript working

--- api.py
+++ api.py
@@ -1,17 +1,28 @@
 from fastapi import FastAPI
 from fastapi import HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+
 from pydantic import BaseModel
+from pydantic import EmailStr
 from s3records import S3Records
 from datetime import datetime
 from typing import Optional
 from typing import List
 from copy import deepcopy
+from cachetools import func
 
+
+import avalanche
 
 S3_STORE = "s3://avymail/records.txt"
 
+@func.ttl_cache(maxsize=1, ttl=3600)
+def get_avalanche_api() -> avalanche.AvalancheAPI:
+    return avalanche.AvalancheAPI()
+
+
 class Recipient(BaseModel):
-    email: str
+    email: EmailStr
     center_id: str
     zone_id: str
     data_last_updated_time: Optional[datetime]
@@ -23,22 +34,48 @@ def remove_timestamps(data: List[Recipient]) -> List[Recipient]:
 
 app = FastAPI()
 
+CORS_ORIGINS = [
+    "https://anthonycannistra.com",
+    "http://anthonycannistra.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=CORS_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+def find_record(r: Recipient, db: List[Recipient]):
+    this_r = deepcopy(r)
+    this_r.data_last_updated_time = None
+    data_copied = deepcopy(db.data)
+    data_copied = remove_timestamps(data_copied)
+    idx = data_copied.index(this_r.dict())
+    return idx
 
 @app.post('/add', status_code=201)
 async def add_recipient(r: Recipient):
     db = S3Records(S3_STORE)
-    db.data.append(r.dict())
-    db.save()
-    return {"message": "success", "recipient": r}
+    try:
+        find_record(r, db)
+    except ValueError:
+        # if ValueError then we don't have sub
+        db.data.append(r.dict())
+        db.save()
+        return {"message": "success", "recipient": r}
+    
+    raise HTTPException(status_code=409, detail="Record already exists.")
+    
 
 @app.post('/remove', status_code=200)
 async def remove_recipient(r: Recipient):
     db = S3Records(S3_STORE)
     try:
-        r.data_last_updated_time = None
-        data_copied = deepcopy(db.data)
-        data_copied = remove_timestamps(data_copied)
-        idx = data_copied.index(r.dict())
+        idx = find_record(r, db)
         del db.data[idx]
         db.save()
     except ValueError:
@@ -46,5 +83,33 @@ async def remove_recipient(r: Recipient):
     
     return {"message": "success"}
     
-        
+@app.get('/subs')
+async def remove_recipient(email: str):
+    db = S3Records(S3_STORE)
+    relevant_records = [r for r in db.data if r['email'] == email]
+    return relevant_records
 
+
+SUPPORTED_ZONES = [
+    "Northwest Avalanche Center",
+    "Central Oregon Avalanche Center",
+    "Sawtooth Avalanche Center",
+    "Bridger-Teton Avalanche Center",
+    "Bridgeport Avalanche Center",
+    "Taos Avalanche Center",
+    "Kachina Peaks Avalanche Center",
+    "Wallowa Avalanche Center",
+    "Flathead Avalanche Center",
+    "Sierra Avalanche Center",
+    "West Central Montana Avalanche Center",
+    "Payette Avalanche Center",
+    "Mount Washington Avalanche Center",
+    "Mount Shasta Avalanche Center",
+    "Idaho Panhandle Avalanche Center",
+    "Eastern Sierra Avalanche Center"
+]
+        
+@app.get('/zones')
+async def zones():
+    a = get_avalanche_api()
+    return {k:v for k,v in a.centers.items() if k in SUPPORTED_ZONES}
---
+++ web/app.js
@@ -0,0 +1,64 @@
+var zones = {}
+
+function setZones() {
+    $("#nac-zone").empty()
+    var selected = $("#nac-center option:selected").text();
+    zones[$("#nac-center option:selected").text()]['zones'].map((e) => $('#nac-zone').append(new Option(e['name'], e['id'])))
+}
+
+$('#nac-center').change(setZones);
+
+fetch("https://avymail.fly.dev/zones")
+    .then((response) => response.json())
+    .then((data) => {
+        $("#nac-center").empty();
+        zones = data;
+        Object.entries(data).map((e) => $("#nac-center").append(new Option(e[0], e[1]['center_id'])));
+        setZones();
+    })
+
+function subscribe() {
+    var center_id = $("#nac-center option:selected").val()
+    var zone_id = $("#nac-zone option:selected").val()
+    var email = $("#email").val()
+    var body = {
+        email: email,
+        center_id: center_id,
+        zone_id: zone_id
+    }
+    console.log(JSON.stringify(body))
+    fetch("http://localhost:8000/add", {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    })
+        .then((res) => {
+            if (res.ok) {
+                return res.json()
+            }
+            return Promise.reject(res)
+        })
+        .then((data) => {
+            $("#postsub-message").text("Success.")
+        })
+        .catch((r) => {
+            console.log(r)
+            r.json().then((json) => {
+                console.log(json);
+                $("#postsub-message").text("Errors: ")
+                if (json['detail'].constructor === Array) {
+                    $("#postsub-message").append(
+                        json['detail'].map((e) => e['msg']).join(', ')
+                    )
+                }
+                else {
+                    $("#postsub-message").append(json['detail'])
+                }
+
+            })
+        })
+}
+
+$("#subscribe").click(subscribe)
